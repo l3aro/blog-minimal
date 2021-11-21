a0fdcfe6037a94fe1419fd94e9e12c819713d054
l3lackheart <dgbao1340@gmail.com>
2021-11-22 00:41:48 +0700

    fix middleware admin routes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'blog.index');
 Route::view('blog/{slug}', 'blog.show')->name('blog.show');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'can:access-admin-function'])->group(function () {
     Route::get('posts', \App\Http\Livewire\Post\Index::class)->name('posts.index');
     Route::get('posts/{post}', \App\Http\Livewire\Post\Show::class)->name('posts.show');
 
@@ -15,7 +15,7 @@ Route::middleware('auth')->group(function () {
     Route::view('users/{user}/edit', 'users.edit')->name('users.edit');
 });
 
-Route::middleware(['auth', 'can:access-admin-function'])->prefix('me')->group(function () {
+Route::middleware('auth')->prefix('me')->group(function () {
     Route::view('/', 'me.index')->name('me.index');
     Route::view('profile', 'me.profile')->name('me.profile');
 
